f3b2cb6ba3dd31d81d838b857b715c87fb9730f7
Syedthousif375 <109578248+Syedthousif375@users.noreply.github.com>
2022-08-18 12:29:20 +0530

    Add files via upload

---
+++ Operation.java
@@ -0,0 +1,12 @@
+class Operation
+  {
+    public static void main(String args[])
+    {
+      int a=20,b=60;
+      System.out.println("the Addition of "+a+" and "+b+" is"+(a+b));
+   System.out.println("the Sabstraction of "+a+" and "+b+" is"+(a-b));
+      System.out.println("the Multipication of "+a+" and "+b+" is"+(a*b));
+      System.out.println("the Division of "+a+" and "+b+" is"+(a/b));
+      System.out.println("the Modolus of "+a+" and "+b+" is"+(a%b));
+    }
+  }
