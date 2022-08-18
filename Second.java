b089b59971de2289dc97db8db4f1b8cbf3a98f24
Syedthousif375 <109578248+Syedthousif375@users.noreply.github.com>
2022-08-18 12:29:39 +0530

    Add files via upload

---
+++ Second.java
@@ -0,0 +1,10 @@
+class Second
+  {
+  public static void main(String args[])
+    {
+    String st="thousif";
+    String st1=new String("Hello");
+    System.out.println("The Given Name is:"+st);
+    System.out.println("The Given String is:"+st1);
+    }
+  }
