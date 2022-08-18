8a5ee5631f939b701482f585ddfc00cd7de43940
Syedthousif375 <109578248+Syedthousif375@users.noreply.github.com>
2022-08-18 12:29:00 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,13 @@
+class Hello
+  {
+public static void main(String args[])
+    {
+  int a=100;
+  int b=200;
+    System.out.println("The a value is:"+a);
+    System.out.println("The b value is:"+b);
+    System.out.println("The addition of two a and b are:"+(a+b));
+   
+}
+}
+ 
