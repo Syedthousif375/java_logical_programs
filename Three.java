e1a928be32566ef73533b848a1ff7c202bd30905
Syedthousif375 <109578248+Syedthousif375@users.noreply.github.com>
2022-08-18 12:58:48 +0530

    Add files via upload

---
+++ Three.java
@@ -0,0 +1,11 @@
+class Three
+  {
+    public static void main(String args[])
+    {
+      float ft=10.23F;
+      double d=2343.43;
+      System.out.println("The Decimal value is:"+ft);
+      System.out.println("The Decimal value is:"+d);
+        
+    }
+  }
