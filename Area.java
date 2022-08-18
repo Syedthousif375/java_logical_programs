56f4291b3f4eb6b9f0019ead5835c87b1531c8b7
Syedthousif375 <109578248+Syedthousif375@users.noreply.github.com>
2022-08-18 12:58:21 +0530

    Add files via upload

---
+++ Area.java
@@ -0,0 +1,11 @@
+class Area
+  {
+    public static void main(String args[])
+    {
+      int r=5;
+      float pi=3.14F,area;
+     System.out.println("The aera of circle is:");
+      area=pi*r*r;
+      System.out.println(area);
+    }
+  }
