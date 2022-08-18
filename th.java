76ecd9cad39fe662cd1a1e3ba5cd0ef8b569cf71
Syedthousif375 <109578248+Syedthousif375@users.noreply.github.com>
2022-08-18 12:30:08 +0530

    Add files via upload

---
+++ th.java
@@ -0,0 +1,14 @@
+class th
+  {
+    public static void main(String args[])
+    {
+      byte eno=101;
+      String ename= "Thousif";
+      double esalary=454.45;
+
+      System.out.println("the employee number is:"+eno);
+      System.out.println("the employee name is:"+ename);
+      System.out.println("the employee salary is:"+esalary);
+    }
+  }
+    
