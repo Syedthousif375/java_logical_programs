e872a036d6ce56d39cf4603567cd029a0637e290
Syedthousif375 <109578248+Syedthousif375@users.noreply.github.com>
2022-08-29 18:46:33 +0530

    Add files via upload

---
+++ table (1).java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class table
+{
+  public static void main(String args[])
+  {
+    Scanner sc=new Scanner(System.in);
+    
+int i, n;
+    System.out.println("enter the n");
+    n=sc.nextInt();
+  
+  for(i=1;i<=10;i++)
+  {
+    System.out.println("\t"+(n*i));
+  }
+  }
+}
