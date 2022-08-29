c2638dca98e6f6340b6c147bd60d41150ce6c9b8
Syedthousif375 <109578248+Syedthousif375@users.noreply.github.com>
2022-08-29 18:43:00 +0530

    Add files via upload

---
+++ h10.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class h10
+{
+  public static void main(String args[])
+  {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("enter the value of i");
+int i;
+  i=sc.nextInt();
+  for(i=10;i>=1;i--)
+  {
+    System.out.println(i);
+  }
+  }
+}
