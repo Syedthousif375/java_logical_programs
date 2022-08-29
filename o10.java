a0a7fa827d4dbd162caee082263f2586d2eba4e0
Syedthousif375 <109578248+Syedthousif375@users.noreply.github.com>
2022-08-29 18:45:16 +0530

    Add files via upload

---
+++ o10.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class o10
+{
+  public static void main(String args[])
+  {
+    Scanner sc=new Scanner(System.in);
+    
+int i, n;
+    System.out.println("enter the n");
+    n=sc.nextInt();
+  
+  for(i=1;i<=n;i++)
+  {
+    System.out.println("\t"+i);
+  }
+  }
+}
