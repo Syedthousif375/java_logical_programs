564b3fe5d0172b0619c88f5711a9d10a811827b3
Syedthousif375 <109578248+Syedthousif375@users.noreply.github.com>
2022-08-30 16:23:43 +0530

    Add files via upload

---
+++ n.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class n
+  {
+    public static void main(String args[])
+    {
+      int i=1,n;
+      Scanner sc=new Scanner(System.in);
+      
+      System.out.println("enter n value:");
+      n=sc.nextInt();
+      
+      while(i<=n)
+        {
+          System.out.println(i);
+          i++;
+          
+        }
+      
+    }
+    
+  }
