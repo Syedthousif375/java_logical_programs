13c6dc5908ca2771750208c13e3f077bb9a0f7cd
Syedthousif375 <109578248+Syedthousif375@users.noreply.github.com>
2022-09-03 17:06:18 +0530

    Add files via upload

---
+++ ew.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class ew
+  {
+    public static void main(String args[])
+    {
+
+    int i=0,n;
+    Scanner sc=new Scanner(System.in);
+    System.out.print("Enter the n even numbers = ");
+     n=sc.nextInt();   
+    while(i<=n)
+      {
+        System.out.println(i);
+        i=i+2;
+      }
+     
+      
+    }
+  }
