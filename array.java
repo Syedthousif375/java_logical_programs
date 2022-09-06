e06a87c550229254f23bfe3ba7f1be6319c45bc3
Syedthousif375 <109578248+Syedthousif375@users.noreply.github.com>
2022-09-06 13:06:58 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,10 @@
+
+class array
+  {
+    public static void main(String args[])
+    {
+      int a[]={10,20,30,40,50};
+      int i;
+      System.out.println("the elements in the array")
+    }
+  }
